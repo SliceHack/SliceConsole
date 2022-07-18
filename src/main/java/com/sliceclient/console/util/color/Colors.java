859dc4e408a937b68240527e15ccf44e15743625
NickRest <nickrestemail@gmail.com>
2022-07-18 14:55:50 -0400

    Color Code

---
+++ src/main/java/com/sliceclient/console/util/color/Colors.java
@@ -0,0 +1,19 @@
+package com.sliceclient.console.util.color;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter @AllArgsConstructor
+public enum Colors {
+    RED("&c", "#FF0000"), GREEN("&a", "#00FF00"),
+    BLUE("&b", "#0000FF"), YELLOW("&e", "#FFFF00"),
+    WHITE("&f", "#FFFFFF"), BLACK("&0", "#000000"),
+    DARK_RED("&4", "#8B0000"), DARK_GREEN("&2", "#006400"),
+    DARK_BLUE("&1", "#00008B"), DARK_YELLOW("&6", "#8B8B00"),
+    DARK_WHITE("&7", "#000000"), GRAY("&8", "#808080"),
+    DARK_GRAY("&9", "#A9A9A9"), LIGHT_RED("&c", "#FFA500"),
+    LIGHT_GREEN("&a", "#90EE90"), LIGHT_BLUE("&b", "#ADD8E6"),
+    LIGHT_YELLOW("&e", "#FFFFE0"), RESET("&r", "#FFFFFF");
+
+    private final String colorCode, hex;
+}
